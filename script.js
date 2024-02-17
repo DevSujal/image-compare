2581d443932e896ca26f1fecb2de527288eaf820
DevSujal <nimjesujal2004@gmail.com>
2024-02-17 18:09:29 +0530

    done the changes

--- script.js
+++ script.js
@@ -14,13 +14,15 @@ function encode() {
         document.getElementById("upload").style.display = "none";
       }
       isfirst = false;
-      newImage.src = srcData;
+      reduce_image_file_size(srcData).then((reduce_img) => {
+        newImage.src = reduce_img;
+      });
     };
     fileReader.readAsDataURL(imageFile);
   }
 }
 
-async function reduce_image_file_size(base64Str, newwidth, newheight) {
+async function reduce_image_file_size(base64Str, newwidth = 0, newheight = 0) {
   let resized_base64 = await new Promise((resolve) => {
     let img = document.createElement("img");
     img.src = base64Str;
@@ -28,82 +30,58 @@ async function reduce_image_file_size(base64Str, newwidth, newheight) {
       let canvas = document.createElement("canvas");
       canvas.width = img.width;
       canvas.height = img.height;
+      let width = newwidth === 0 ? img.width : newwidth
+      let height = newheight === 0 ? img.height : newheight
       let ctx = canvas.getContext("2d");
-      ctx.drawImage(img, 0, 0, newwidth, newheight);
-      resolve(canvas.toDataURL("image/webp", 100)); // this will return base64 image results after resize
+      ctx.drawImage(img, 0, 0, width, height);
+      resolve(canvas.toDataURL("image/webp", 100)); 
     };
   });
   return resized_base64;
 }
 
-function lcs(string1, string2) {
-  let m = string1.length + 1;
-  let n = string2.length + 1;
+function lcs(X, Y) {
+  let m = X.length,
+    n = Y.length;
 
-  let c = [];
-  for (let i = 0; i < m; i++) {
-    c[i] = [];
-    for (let j = 0; j < n; j++) {
-      c[i][j] = { val: 0, dir: "H" };
+  let L = new Array(2);
+  for (let i = 0; i < 2; i++) {
+    L[i] = new Array(n + 1);
+    for (let j = 0; j < n + 1; j++) {
+      L[i][j] = 0;
     }
   }
+  let bi = 0;
 
-  let stringArr1 = [" ", ...string1];
-  let stringArr2 = [" ", ...string2];
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      if (stringArr1[i] !== stringArr2[j]) {
-        c[i][j].val = Math.max(c[i - 1][j].val, c[i][j - 1].val);
-        if (c[i - 1][j].val >= c[i][j - 1].val) {
-          c[i][j].dir = "U";
-        } else {
-          c[i][j].dir = "S";
-        }
-      } else {
-        c[i][j].val = c[i - 1][j - 1].val + 1;
-        c[i][j].dir = "D";
-      }
+  for (let i = 0; i <= m; i++) {
+    bi = i & 1;
+
+    for (let j = 0; j <= n; j++) {
+      if (i == 0 || j == 0) L[bi][j] = 0;
+      else if (X[i - 1] == Y[j - 1]) {
+        L[bi][j] = L[1 - bi][j - 1] + 1;
+      } else L[bi][j] = Math.max(L[1 - bi][j], L[bi][j - 1]);
     }
   }
-  // console.log(c);
-  return { lcs: c, lcsval: getLcsValue(c, m - 1, n - 1) };
-}
-
-function getLcsValue(c, m, n) {
-  if (m == 0 || n == 0) {
-    return 0;
-  }
-  if (c[m][n].dir == "D") {
-    return c[m][n].val;
-  } else {
-    if (c[m][n].dir == "U") {
-      return getLcsValue(c, m - 1, n);
-    } else {
-      return getLcsValue(c, m, n - 1);
-    }
-  }
+  return L[bi][n];
 }
 
 function search() {
-  let text = document.getElementById("ans")
-  text.innerText = "Comparing...\n"
-  image1 = document.getElementById("image1").src;
-  image2 = document.getElementById("image2").src;
-  console.log(image1.length, image2.length);
+  let text = document.getElementById("ans");
+  text.innerText = "Comparing...\n";
+  let image1 = document.getElementById("image1").src;
+  let image2 = document.getElementById("image2").src;
 
-  reduce_image_file_size(image1, 10, 10).then((reduce_file) => {
-    image1 = reduce_file
-    reduce_image_file_size(image2, 10, 10).then((reduce_file) => {
-      image2 = reduce_file
-      image1 = image1.split(",")[1];
-      image2 = image2.split(",")[1];
+  reduce_image_file_size(image1, 20, 20).then((reduce_file) => {
+    image1 = reduce_file;
+    reduce_image_file_size(image2, 20, 20).then((reduce_file) => {
+      image2 = reduce_file;
       lcsvalue = lcs(image1, image2);
-    
       let maxlength = Math.max(image1.length, image2.length);
       text.innerText = `The Above Two Images Are matching ${(
-        (lcsvalue.lcsval / maxlength) *
+        (lcsvalue / maxlength) *
         100
       ).toFixed(2)} %`;
-    })
-  })
+    });
+  });
 }
